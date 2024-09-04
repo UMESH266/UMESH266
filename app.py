e5b977b23b2756eceabc05fc4b235481a4f5263a
UMESH266 <144141198+UMESH266@users.noreply.github.com>
2024-09-04 13:03:29 +0530

    spreadsheet added

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ elif choice == "Contact":
         st.session_state.conn = st.connection("gsheets", type=GSheetsConnection)
 
     if "Message_df" not in st.session_state:
-       st.session_state.Message_df = st.session_state.conn.read(spreadsheet="Portfolio feedback", worksheet="Feedback")
+       st.session_state.Message_df = st.session_state.conn.read(spreadsheet="Profile_feedback", worksheet="Feedback")
 
     if "msg_df" not in st.session_state:
        st.session_state.msg_df = pd.DataFrame()
